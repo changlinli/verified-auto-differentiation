eac669735cc4e8856f991e2949e7fa47e9fefc2a
dupe <njdupe@gmail.com>
2023-10-24 18:01:01 -0400

    wip: started proof on simple functions

--- using_real_nums.v
+++ using_real_nums.v
@@ -69,13 +69,13 @@ Definition differentiable_at (f : R -> R) (x : R) := derivable_pt f x.
 
 Definition differentiable (f : R -> R) := derivable f.
 
+Inductive dual_num := mk_dual (num : R) (deriv : R).
+
 Inductive auto_diff_ast :=
-  | Var (x : R)
+  | Var
   | Constant (x : auto_diff_ast) (c : R)
   | Add (x y : auto_diff_ast)
   | Subtract (x y : auto_diff_ast).
-
-Inductive dual_num := mk_dual (num : R) (deriv : R).
 
 Definition constant_dual (r : R) (_ : dual_num) : dual_num :=
   mk_dual r 0.
@@ -85,27 +85,52 @@ Definition add_dual (x y : dual_num) : dual_num :=
   | (mk_dual x_val x_deriv), (mk_dual y_val y_deriv) => mk_dual (x_val + y_val) (x_deriv + y_deriv)
   end.
 
-(* 
+Definition subtract_dual (x y : dual_num) : dual_num :=
+  match x, y with
+  | (mk_dual x_val x_deriv), (mk_dual y_val y_deriv) => mk_dual (x_val - y_val) (x_deriv - y_deriv)
+  end.
+
+(*
 Definition my_ast : auto_diff_ast.
 
 Definition my_f : dual_num -> dual_num := eval_ast_dual my_ast.
+    Add
+      Var
+        x
+      Var
+        x
 *)
 
 Fixpoint eval_ast_dual (ast : auto_diff_ast) (x : dual_num) : dual_num :=
   match ast with
   | Add x_ast y_ast => add_dual (eval_ast_dual x_ast x) (eval_ast_dual y_ast x)
-  | Var _ => _
+  | Var => x
   | Constant x_ast c => constant_dual c (eval_ast_dual x_ast x)
-  | Subtract _ _ => _
+  | Subtract x_ast y_ast => subtract_dual (eval_ast_dual x_ast x) (eval_ast_dual y_ast x)
   end.
 
-Definition eval_ast_value (ast : auto_diff_ast) (x : R) : R. Admitted.
+Definition eval_ast_value (ast : auto_diff_ast) (x : R) : R :=
+  match (eval_ast_dual ast (mk_dual x 1)) with
+  | mk_dual num deriv => num
+  end.
 
-Definition eval_ast_derivative (ast : auto_diff_ast) (x : R) : R. Admitted.
+Definition eval_ast_derivative (ast : auto_diff_ast) (x : R) : R :=
+  match (eval_ast_dual ast (mk_dual x 1)) with
+  | mk_dual num deriv => deriv
+  end.
 
-Definition eval_value (f : dual_num -> dual_num) (x : R) : R. Admitted.
+Compute eval_ast_value (Add Var Var) 2.
+Compute eval_ast_derivative (Add Var Var) 2.
 
-Definition eval_derivative (f : dual_num -> dual_num) (x : R) : R. Admitted.
+Definition eval_value (f : dual_num -> dual_num) (x : R) : R :=
+  match (f (mk_dual x 1)) with
+  | mk_dual x _ => x
+  end.
+
+Definition eval_derivative (f : dual_num -> dual_num) (x : R) : R :=
+  match (f (mk_dual x 1)) with
+  | mk_dual _ d => d
+  end.
 
 Definition is_well_formed (f : dual_num -> dual_num) : Prop :=
   exists ast : auto_diff_ast, eval_ast_dual ast = f.
@@ -115,6 +140,97 @@ Definition derivative_is_correct (f_dual : dual_num -> dual_num) : Prop :=
   let f' := eval_derivative f_dual in
   forall x : R, differentiable_at f x -> derivative_at_point_is f x (f' x).
 
+Axiom functional_extensionality : forall {X Y: Type}
+                                    {f g : X -> Y},
+  (forall (x:X), f x = g x) -> f = g.
+
+Lemma eval_value_id_is_id : eval_value (fun x => x) = id.
+Proof.
+unfold eval_value.
+reflexivity.
+Qed.
+
+Lemma eval_deriv_id_is_1 : forall r : R, eval_derivative (fun x => x) r = 1.
+Proof.
+  unfold eval_derivative.
+  reflexivity.
+Qed.
+
+Lemma eval_value_const_is_const :
+  forall c,
+  eval_value (constant_dual c) = fct_cte c.
+Proof.
+  intros c.
+  unfold eval_value.
+  simpl.
+  reflexivity.
+Qed.
+
+Lemma eval_deriv_const_is_0 : forall c r, eval_derivative (constant_dual c) r = 0.
+Proof.
+  unfold eval_derivative.
+  reflexivity.
+Qed.
+
+Lemma constant_ignores_arg : forall c x,
+  (fun x1 : dual_num => constant_dual c (eval_ast_dual x x1)) = (constant_dual c).
+Proof.
+  intros c x.
+  unfold constant_dual.
+  reflexivity.
+Qed.
+
 Theorem auto_differentiate_is_correct :
   forall f : dual_num -> dual_num, is_well_formed f -> derivative_is_correct f.
-  Admitted.
+Proof.
+  intros f H.
+  destruct H.
+  induction x.
+  - unfold derivative_is_correct.
+    rewrite <- H.
+    simpl.
+    intros x Hdiff.
+    rewrite eval_value_id_is_id.
+    rewrite eval_deriv_id_is_1.
+    apply derivable_pt_lim_id.
+  - unfold derivative_is_correct.
+    rewrite <- H.
+    simpl.
+    rewrite constant_ignores_arg.
+    intros x0 Hdiff.
+    rewrite eval_value_const_is_const.
+    rewrite eval_deriv_const_is_0.
+    apply derivable_pt_lim_const.
+  -
+Admitted.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
