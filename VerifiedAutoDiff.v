35c95061de2eb9e61f2adeefd30958676ee6ff8b
Changlin Li <mail@changlinli.com>
2023-10-26 17:21:29 -0700

    Delete unused Lemmas

--- VerifiedAutoDiff.v
+++ VerifiedAutoDiff.v
@@ -165,154 +165,6 @@ Definition derivative_is_correct' (f_dual : dual_num -> dual_num) : Prop :=
   let f' := eval_derivative f_dual in
   forall x : R, differentiable_at f x -> derivative_at_point_is f x (f' x).
 
-Lemma eval_value_id_is_id : eval_value (fun x => x) = id.
-Proof.
-unfold eval_value.
-reflexivity.
-Qed.
-
-Lemma eval_deriv_id_is_1 : forall r : R, eval_derivative (fun x => x) r = 1.
-Proof.
-  unfold eval_derivative.
-  reflexivity.
-Qed.
-
-Lemma eval_value_const_is_const :
-  forall c,
-  eval_value (constant_dual c) = fct_cte c.
-Proof.
-  intros c.
-  unfold eval_value.
-  simpl.
-  reflexivity.
-Qed.
-
-Lemma eval_deriv_const_is_0 : forall c r, eval_derivative (constant_dual c) r = 0.
-Proof.
-  unfold eval_derivative.
-  reflexivity.
-Qed.
-
-Lemma constant_ignores_arg : forall c x,
-  (fun x1 : dual_num => constant_dual c (eval_ast_dual x x1)) = (constant_dual c).
-Proof.
-  intros c x.
-  unfold constant_dual.
-  reflexivity.
-Qed.
-
-Lemma eval_value_add_is_add :
-  forall f g : dual_num -> dual_num,
-  eval_value (fun x => add_dual (f x) (g x)) =
-    fun x => eval_value f x + eval_value g x.
-Proof.
-reflexivity.
-Qed.
-
-Lemma eval_deriv_add_is_add :
-  forall f g : dual_num -> dual_num,
-  eval_derivative (fun x => add_dual (f x) (g x)) =
-    fun x => eval_derivative f x + eval_derivative g x.
-Proof.
-reflexivity.
-Qed.
-
-Lemma eval_value_subtract_is_subtract :
-  forall f g : dual_num -> dual_num,
-  eval_value (fun x => subtract_dual (f x) (g x)) =
-    fun x => eval_value f x - eval_value g x.
-Proof.
-reflexivity.
-Qed.
-
-Lemma eval_deriv_subtract_is_subtract :
-  forall f g : dual_num -> dual_num,
-  eval_derivative (fun x => subtract_dual (f x) (g x)) =
-    fun x => eval_derivative f x - eval_derivative g x.
-Proof.
-reflexivity.
-Qed.
-
-Lemma eval_value_multiply_is_multiply :
-  forall f g : dual_num -> dual_num,
-    eval_value (fun x => multiply_dual (f x) (g x)) =
-      fun x => eval_value f x * eval_value g x.
-Proof.
-reflexivity.
-Qed.
-
-Lemma eval_deriv_multiply_is_multiply_and_add :
-  forall f g : dual_num -> dual_num,
-    eval_derivative (fun x => multiply_dual (f x) (g x)) =
-      fun x => eval_derivative f x * eval_value g x + eval_value f x * eval_derivative g x.
-Proof.
-reflexivity.
-Qed.
-
-Lemma eval_value_sin_is_sin :
-  eval_value sin_dual = sin.
-Proof.
-reflexivity.
-Qed.
-
-Lemma one_mult_cos_is_still_cos : 
-  forall x : R, 1 * cos x = cos x.
-Proof.
-intros.
-rewrite Rmult_1_l.
-reflexivity.
-Qed.
-
-Lemma eval_deriv_sin_is_cos :
-  eval_derivative sin_dual = fun x => cos x.
-Proof.
-unfold sin_dual.
-unfold eval_derivative.
-unfold dual_deriv.
-unfold dual_value.
-rewrite (functional_extensionality (fun x => 1 * cos x) cos one_mult_cos_is_still_cos).
-reflexivity.
-Qed.
-
-(*
-Lemma eval_value_cos_is_cos :
-  eval_value cos_dual = cos.
-Proof.
-reflexivity.
-Qed.
-
-Lemma one_mult_neg_sin_is_still_neg_sin :
-  forall x : R, -1 * 1 * sin x = -1 * sin x.
-Proof.
-intros.
-rewrite Rmult_1_r.
-reflexivity.
-Qed.
-
-Lemma eval_deriv_cos_is_negative_sin :
-  eval_derivative cos_dual = fun x => -1 * sin x.
-Proof.
-unfold cos_dual.
-unfold eval_derivative.
-unfold dual_deriv.
-unfold dual_value.
-rewrite (functional_extensionality (fun x : R => -1 * 1 * sin x) (fun x : R => -1 * sin x) one_mult_neg_sin_is_still_neg_sin).
-reflexivity.
-Qed.
-*)
-
-Lemma eval_value_tan_is_tan :
-  eval_value tan_dual = tan.
-Proof.
-reflexivity.
-Qed.
-
-Lemma eval_deriv_tan_is_acos_squared :
-  eval_derivative tan_dual = fun x => 1 / (cos x ^ 2).
-Proof.
-reflexivity.
-Qed.
-
 Theorem derivative_of_tan : forall x : R, derivative_at_point_is tan x (1 / (cos x) ^ 2).
 Admitted.
 
@@ -331,16 +183,11 @@ Proof.
     intros.
     rewrite <- H.
     simpl.
-    rewrite eval_value_id_is_id.
-    rewrite eval_deriv_id_is_1.
     apply derivable_pt_lim_id.
   - unfold derivative_is_correct.
     intros.
     rewrite <- H.
     simpl.
-    rewrite constant_ignores_arg.
-    rewrite eval_value_const_is_const.
-    rewrite eval_deriv_const_is_0.
     apply derivable_pt_lim_const.
   - intros.
     rewrite <- H.
@@ -349,8 +196,6 @@ Proof.
     unfold derivative_is_correct in IHx1.
     unfold derivative_is_correct in IHx2.
     intros.
-    rewrite eval_value_add_is_add.
-    rewrite eval_deriv_add_is_add.
     pose proof (IHx1 (eval_ast_dual x1) eq_refl x).
     pose proof (IHx2 (eval_ast_dual x2) eq_refl x).
     apply (derivable_pt_lim_plus _ _ _ _ _ H0 H1).
@@ -361,8 +206,6 @@ Proof.
     unfold derivative_is_correct in IHx1.
     unfold derivative_is_correct in IHx2.
     intros.
-    rewrite eval_value_subtract_is_subtract.
-    rewrite eval_deriv_subtract_is_subtract.
     pose proof (IHx1 (eval_ast_dual x1) eq_refl x).
     pose proof (IHx2 (eval_ast_dual x2) eq_refl x).
     apply (derivable_pt_lim_minus _ _ _ _ _ H0 H1).
@@ -373,8 +216,6 @@ Proof.
     unfold derivative_is_correct in IHx1.
     unfold derivative_is_correct in IHx2.
     intros.
-    rewrite eval_value_multiply_is_multiply.
-    rewrite eval_deriv_multiply_is_multiply_and_add.
     pose proof (IHx1 (eval_ast_dual x1) eq_refl x).
     pose proof (IHx2 (eval_ast_dual x2) eq_refl x).
     apply (derivable_pt_lim_mult _ _ _ _ _ H0 H1).
@@ -383,10 +224,6 @@ Proof.
     simpl.
     unfold derivative_is_correct.
     intros.
-    unfold eval_value.
-    simpl.
-    unfold eval_derivative.
-    simpl.
     pose proof (IHx (eval_ast_dual x) eq_refl x0).
     pose proof (derivable_pt_lim_sin (eval_value (eval_ast_dual x) x0)) as sin_deriv.
     pose proof (derivable_pt_lim_comp _ _ _ _ _ H0 sin_deriv) as chain_rule_deriv.
@@ -397,10 +234,6 @@ Proof.
     simpl.
     unfold derivative_is_correct.
     intros.
-    unfold eval_value.
-    simpl.
-    unfold eval_derivative.
-    simpl.
     pose proof (IHx (eval_ast_dual x) eq_refl x0).
     pose proof (derivable_pt_lim_cos (eval_value (eval_ast_dual x) x0)) as cos_deriv.
     pose proof (derivable_pt_lim_comp _ _ _ _ _ H0 cos_deriv) as chain_rule_deriv.
