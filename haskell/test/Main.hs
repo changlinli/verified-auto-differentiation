7e27edfacbd86ef6bccb1f4c07ffef025105f136
Changlin Li <mail@changlinli.com>
2023-11-01 13:49:34 -0700

    Add dummy test suite

--- haskell/test/Main.hs
+++ haskell/test/Main.hs
@@ -1,4 +1,44 @@
 module Main (main) where
 
+import qualified Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+
+import Test.Tasty.Hedgehog as HH
+
+import Data.Function ((&))
+import Hedgehog ((===))
+import Test.Tasty (TestTree, defaultMain, testGroup)
+import Test.Tasty.HUnit (testCase, (@?=))
+
+tests :: TestTree
+tests = testGroup "Tests"
+  [ properties
+  , unitTests
+  ]
+
+properties :: TestTree
+properties = testGroup "Properties"
+  [ hedgehogProperties
+  ]
+
+hedgehogProperties :: TestTree
+hedgehogProperties = testGroup "(checked by Hedgehog)"
+  [ HH.testProperty "dummy property" $
+      dummyProperty
+  ]
+
+dummyProperty :: Hedgehog.Property
+dummyProperty =
+    Hedgehog.property $ do
+        x <- Hedgehog.forAll $ Gen.int (Range.linear 1 10)
+        x === x
+
+unitTests :: TestTree
+unitTests = testGroup "Unit tests"
+  [ testCase "dummy unit test" $
+      1 @?= 1
+  ]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = defaultMain tests
